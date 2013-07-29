ff79b7d19e785f8041f4136ac013553348fa47be
Jameson Nuss <jamesonnuss@yahoo.com>
2013-07-29 11:14:11 -0700

    Errors

--- AddThis_Social/AddThis_Social.php
+++ AddThis_Social/AddThis_Social.php
@@ -10,18 +10,15 @@ License: GNU General Public License v2 or later
 */
 
 function AddThis_enqueue_scripts(){
-	if ( is_single() && 0 == get_option( ' pimple_disable_button',0) ) {
+	if ( is_single() && 0 == get_option( ' AddThis_disable_button',0) ) {
 	wp_enqueue_script(
-		'addThis - Social',
+		'AddThis',
 		'//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6a88714752a90',
-		array(
-			'jquery',
-			'underscore'
-		),
+		array(),
 		null,
 		true
 
-	);
+		);
 
 	}
 
@@ -50,7 +47,7 @@ function AddThis_add_button( $content ){
 	return $content;
 }
 
-add_filter('the_content', 'AddThis_add_button', 1);
+add_filter('the_content', 'AddThis_add_button', 20);
 
 	//Recieved Help From Chris Allen
 
