b1a5bb364e422fdb41362630c5136991d96371de
Jameson Nuss <jamesonnuss@yahoo.com>
2013-07-29 11:10:51 -0700

    More Errors

--- AddThis_Social/AddThis_Social.php
+++ AddThis_Social/AddThis_Social.php
@@ -13,7 +13,7 @@ function AddThis_enqueue_scripts(){
 	if ( is_single() && 0 == get_option( ' pimple_disable_button',0) ) {
 	wp_enqueue_script(
 		'addThis - Social',
-		'//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6a88714752a90'
+		'//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51f6a88714752a90',
 		array(
 			'jquery',
 			'underscore'
